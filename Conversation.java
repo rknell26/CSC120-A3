9829371f9ff17124f7197d1b8d859109a230ed47
Rachel Knell <rknell@smith.edu>
2023-03-06 03:24:00 -0500

    First Draft

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,37 @@
+
+import java.util.Scanner;
+
 class Conversation {
 
+
+
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    int rounds;
+    Scanner in;
+    int thisRound = 0;
+    String line;
+    //String[] words;
+
+ 
+    in = new Scanner(System.in);
+    System.out.print("How many rounds do you want to chat for? ");
+    rounds = in.nextInt();
+    System.out.println("Greetings, earthling! What's on your mind? ");
+
+    
+    while (rounds != thisRound) {
+      //words = new String[0];
+      line = in.nextLine();
+      //words = line.split(" ");
+      thisRound += 1;
+    }
+    
+  
+
+    }
+
+
+    
   }
-}
+
+
